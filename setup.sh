ab50feef3034d23ccb34771dbaf7aa494153becb
nops <0x9090@users.noreply.github.com>
2017-07-29 22:35:33 -0700

    setup improvements

--- setup.sh
+++ setup.sh
@@ -5,9 +5,12 @@ if [ "$EUID" -ne 0 ]; then
 	exit
 fi
 
+current_path=${PWD##*/}
+
 apt update && apt upgrade -y
 apt install sudo puppet unzip -y
 
-mkdir /opt/puppet
-cd /opt/puppet/
-#wget https://github.com/puppetlabs/puppetlabs-stdlib/archive/4.17.1.zip
+mkdir /opt/puppet /opt/puppet/modules /opt/puppet/manifests
+cd /opt/puppet/modules
+wget https://github.com/puppetlabs/puppetlabs-stdlib/archive/4.17.1.zip && unzip 4.17.1.zip
+#wget https://
